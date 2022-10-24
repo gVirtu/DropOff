6ddc7184f6495e9ebae2e9afd0a454a518039539
Quipex <rudas72@gmail.com>
2022-10-25 00:15:32 +0300

    1.19: * refactoring + wrapping inside enqueueWork

--- src/main/java/tfar/quickstack/networking/C2SPacketRequestDropoff.java
+++ src/main/java/tfar/quickstack/networking/C2SPacketRequestDropoff.java
@@ -14,18 +14,16 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.wrapper.InvWrapper;
 import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.PacketDistributor;
-import tfar.quickstack.DropOff;
 import tfar.quickstack.client.RendererCubeTarget;
 import tfar.quickstack.config.DropOffConfig;
-import tfar.quickstack.util.InventoryData;
 import tfar.quickstack.util.ItemStackUtils;
+import tfar.quickstack.util.SuccedableInventoryData;
 
 public class C2SPacketRequestDropoff {
 
@@ -58,48 +56,49 @@ public class C2SPacketRequestDropoff {
     }
 
     public void handle(Supplier<NetworkEvent.Context> ctx) {
-        ServerPlayer player = ctx.get().getSender();
-        Set<InventoryData> nearbyInventories = getNearbyInventories(player);
+        ctx.get().enqueueWork(() -> {
+            ServerPlayer player = ctx.get().getSender();
+            Set<SuccedableInventoryData> nearbyInventories = getNearbyInventories(player);
 
-        nearbyInventories.forEach(data -> {
-            BlockEntity blockEntity = player.level.getBlockEntity(data.pos);
-            if (blockEntity == null) {
-                return;
+            nearbyInventories.forEach(inventoryData -> {
+                inventoryData.blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER)
+                        .ifPresent(
+                                target -> {
+                                    if (dump) {
+                                        dropOff(player, target, inventoryData);
+                                    } else {
+                                        dropOffExisting(player, target, inventoryData);
+                                    }
+                                });
+            });
+
+            List<RendererCubeTarget> rendererCubeTargets = new ArrayList<>();
+            int affectedContainers = 0;
+            player.containerMenu.broadcastChanges();
+
+            for (SuccedableInventoryData inventoryData : nearbyInventories) {
+                int color;
+
+                if (inventoryData.success) {
+                    affectedContainers++;
+                    color = 0x00FF00;
+                } else {
+                    color = 0xFF0000;
+                }
+
+                RendererCubeTarget rendererCubeTarget = new RendererCubeTarget(inventoryData.blockEntity.getBlockPos(),
+                        color);
+                rendererCubeTargets.add(rendererCubeTarget);
             }
-            blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(
-                    target -> {
-                        if (dump)
-                            dropOff(player, target, data);
-                        else
-                            dropOffExisting(player, target, data);
-                    });
+
+            PacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player),
+                    new S2CReportPacket(itemsCounter, affectedContainers, nearbyInventories.size(),
+                            rendererCubeTargets));
         });
-
-        List<RendererCubeTarget> rendererCubeTargets = new ArrayList<>();
-        int affectedContainers = 0;
-        player.containerMenu.broadcastChanges();
-
-        for (InventoryData inventoryData : nearbyInventories) {
-            int color;
-
-            if (inventoryData.success) {
-                affectedContainers++;
-                color = 0x00FF00;
-            } else {
-                color = 0xFF0000;
-            }
-
-            RendererCubeTarget rendererCubeTarget = new RendererCubeTarget(inventoryData.pos, color);
-            rendererCubeTargets.add(rendererCubeTarget);
-        }
-
-        PacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player),
-                new S2CReportPacket(itemsCounter, affectedContainers, nearbyInventories.size(),
-                        rendererCubeTargets));
         ctx.get().setPacketHandled(true);
     }
 
-    public void dropOff(Player player, IItemHandler target, InventoryData data) {
+    public void dropOff(Player player, IItemHandler target, SuccedableInventoryData data) {
         IItemHandler playerstacks = new InvWrapper(player.getInventory());
         for (int i = 0; i < 36; ++i) {
             if (ignoreHotbar && i < 9)
@@ -123,8 +122,7 @@ public class C2SPacketRequestDropoff {
         }
     }
 
-    public void dropOffExisting(Player player, IItemHandler target, InventoryData data) {
-
+    public void dropOffExisting(Player player, IItemHandler target, SuccedableInventoryData data) {
         IItemHandler playerstacks = new InvWrapper(player.getInventory());
         for (int i = 0; i < 36; ++i) {
             if (ignoreHotbar && i < 9)
@@ -152,14 +150,10 @@ public class C2SPacketRequestDropoff {
         }
     }
 
-    public Set<InventoryData> getNearbyInventories(ServerPlayer player) {
+    public Set<SuccedableInventoryData> getNearbyInventories(ServerPlayer player) {
         double playerX = player.position().x;
         double playerY = player.position().y;
         double playerZ = player.position().z;
-        DropOff.LOGGER.debug("Scanning inventories near player {}, coords: {} {} {}", player.getName().getString(),
-                playerX,
-                playerY, playerZ);
-
         int minX = (int) (playerX - DropOffConfig.scanRadius.get());
         int maxX = (int) (playerX + DropOffConfig.scanRadius.get());
 
@@ -169,35 +163,15 @@ public class C2SPacketRequestDropoff {
         int minZ = (int) (playerZ - DropOffConfig.scanRadius.get());
         int maxZ = (int) (playerZ + DropOffConfig.scanRadius.get());
 
-        Level world = player.level;
-        DropOff.LOGGER.debug("World info: {}", world);
-        DropOff.LOGGER.debug("Is client: {}", world.isClientSide());
-
-        DropOff.LOGGER.debug("Scanning x({},{}) y({},{}), z({},{})", minX, maxX, minY, maxY, minZ, maxZ);
-        var blockEntities = BlockPos.betweenClosedStream(minX, minY, minZ, maxX, maxY, maxZ)
-                .map(world::getBlockEntity)
-                .toList();
-        DropOff.LOGGER.debug("Found {} block entities", blockEntities.size());
-
-        var nonNulls = blockEntities.stream()
-                .filter(Objects::nonNull).toList();
-        DropOff.LOGGER.debug("Found {} non-nulls", nonNulls.size());
-
-        var hasSlotsItemHandlers = nonNulls.stream()
+        Level world = player.getLevel();
+        return BlockPos.betweenClosedStream(minX, minY, minZ, maxX, maxY, maxZ)
+        .map(world::getBlockEntity)
+        .filter(Objects::nonNull)
                 .filter(tileEntity -> tileEntity.getCapability(ForgeCapabilities.ITEM_HANDLER)
                         .filter(iItemHandler -> iItemHandler.getSlots() >= minSlotCount)
                         .isPresent())
-                .toList();
-        DropOff.LOGGER.debug("Found {} item handlers with slots >= {}", hasSlotsItemHandlers.size(),
-                minSlotCount);
-
-        var ofAllowedType = hasSlotsItemHandlers.stream()
-                .filter(tileEntity -> !teTypes.contains(tileEntity.getType())).toList();
-        DropOff.LOGGER.debug("Found {} containers of allowed type", ofAllowedType.size());
-
-        return ofAllowedType.stream()
-                .map(BlockEntity::getBlockPos)
-                .map(InventoryData::new)
+                .filter(tileEntity -> !teTypes.contains(tileEntity.getType()))
+                .map(SuccedableInventoryData::new)
                 .collect(Collectors.toSet());
     }
 
